3062a42c182b1e8d59941da36bec2ed7c7b30dfe
GMDIV <mitchelld20@yahoo.com>
2017-01-12 04:31:57 -0600

    increased form options available

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ var app = angular.module('minmax', []);
 
 app.controller('MinMaxCtrl', function($scope){
 	$scope.formModel = {};
+
+	$scope.onSubmit = function(){
+		console.log("The submit button has been clicked!");
+		console.log($scope.formModel);
+	}
 	
 })
