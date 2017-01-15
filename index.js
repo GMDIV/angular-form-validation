8beeca782abad8fde6cba0bb6ccff7dadba244ed
Mitchell Deane <mitchelld20@yahoo.com>
2017-01-14 20:22:48 -0600

    added various forms of validation and testing validation

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var app = angular.module('minmax', []);
 app.controller('MinMaxCtrl', function($scope){
 	$scope.formModel = {};
 
-	$scope.onSubmit = function(){
-		console.log("The submit button has been clicked!");
-		console.log($scope.formModel);
+	$scope.onSubmit = function(valid){
+	
+		if(valid) {
+			console.log("The submit button has been clicked!");
+			console.log($scope.formModel);
+		}
+		else {
+			console.log("Invalid Form! Fill out all of the information required!")
+		}
 	}
 	
 })
