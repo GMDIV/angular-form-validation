2345860eecdca81f21f9113b62f7884798015622
Mitchell Deane <mitchelld20@yahoo.com>
2017-01-14 20:49:22 -0600

    added angular-auto-validate to improve and simplify form validation.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var app = angular.module('minmax', []);
+var app = angular.module('minmax', [
+   'jcs-autoValidate'
 
+]);
+
+app.run(function(defaultErrorMessageResolver){
+	defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages){
+		errorMessages['tooYoung']= 'You must be at least {0} years old to use this website';
+		errorMessages['tooOld']= 'You must be max {0} years old to use this website';
+		errorMessages['badUserName']= 'Username can only consist of letters and numbers and "_" ';
+	})
+});
 app.controller('MinMaxCtrl', function($scope){
 	$scope.formModel = {};
 
-	$scope.onSubmit = function(valid){
+	$scope.onSubmit = function(){
 	
-		if(valid) {
+		
 			console.log("The submit button has been clicked!");
 			console.log($scope.formModel);
-		}
-		else {
-			console.log("Invalid Form! Fill out all of the information required!")
-		}
+		
+
 	}
 	
 })
